24742c0a7d33c71ffba0fe9794c96d85341406e3
Sharlon N. Regales <sharlonregales@gmail.com>
2025-10-22 09:17:47 +0200

    fix flake to work with devenv

--- flake.nix
+++ flake.nix
@@ -1,25 +1,53 @@
-# SPDX-License-Identifier: Unlicense
 {
   inputs = {
-    # nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    # systems.url = "github:nix-systems/default";
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv";
+    devenv.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = {nixpkgs, ...}: let
-    eachSystem = f:
-      nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (system: f nixpkgs.legacyPackages.${system});
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    devenv,
+    systems,
+    ...
+  } @ inputs: let
+    forEachSystem = nixpkgs.lib.genAttrs (import systems);
   in {
-    devShells = eachSystem (pkgs: {
-      default = pkgs.mkShell {
-        buildInputs = [
-          pkgs.git
-          pkgs.lazygit
-          pkgs.python313
-          pkgs.uv
-          pkgs.ruff
-          pkgs.just
-        ];
-      };
-    });
+    devShells =
+      forEachSystem
+      (system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        default = devenv.lib.mkShell {
+          inherit inputs pkgs;
+          modules = [
+            {
+              # https://devenv.sh/reference/options/
+              packages = with pkgs; [
+                git
+                lazygit
+                just
+                python313
+                uv
+                pdm
+                ruff
+              ];
+
+              enterShell = ''
+                just install
+                pdm config use_uv true
+                pdm config python.install_root $(uv python dir --color never)
+              '';
+            }
+          ];
+        };
+      });
   };
 }
