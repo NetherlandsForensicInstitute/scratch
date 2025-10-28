d6886379ba243ee4ed83efad7136caecc5765996
Leen van der Ham <l.van.der.ham@nfi.nl>
2025-10-28 17:49:52 +0100

    Work in progress

--- lrmodule/mcmc/lr_analyses.py
+++ lrmodule/mcmc/lr_analyses.py
@@ -3,50 +3,81 @@ import numpy as np
 import pymc as pm
 from scipy.stats import binom, betabinom
 
+
 def analyse_bayesian(scores_eval, scores_km, scores_knm, stats_model):
 
-    probs_km = run_mcmc_sims(scores_eval, scores_km, stats_model.km, stats_model.mcmc_settings)
-    probs_knm = run_mcmc_sims(scores_eval, scores_knm, stats_model.knm, stats_model.mcmc_settings)
+    parameters_km = fit_mcmc(stats_model.km, scores_km, stats_model.mcmc_settings)
+    parameters_knm = fit_mcmc(stats_model.knm, scores_knm, stats_model.mcmc_settings)
+
+    probs_km = apply_mcmc(stats_model.km, parameters_km, scores_eval)
+    probs_knm = apply_mcmc(stats_model.knm, parameters_knm, scores_eval)
+
     llrs = np.log10(probs_km) - np.log10(probs_knm)
 
     return llrs, probs_km, probs_knm
 
-def run_mcmc_sims(scores_eval, scores_obs, stats_model, mcmc_settings):
 
-    sample_count = mcmc_settings.chain_count * mcmc_settings.draw_count
-    k_obs_2d = np.tile(np.expand_dims(scores_eval[:,0], 1), (1, sample_count))
-    n_obs_2d = np.tile(np.expand_dims(scores_eval[:,1], 1), (1, sample_count))
+def fit_mcmc(stats_model, scores_obs, mcmc_settings):
+
+    parameters = list(stats_model['parameters'])
 
     with pm.Model():
+        # Define the prior distributions of the model parameters
+        priors = {}
+        for parameter in parameters:
+            parameter_input = stats_model.parameters[parameter]
+            if parameter_input.prior == 'uniform':
+                prior = pm.Uniform(parameter, parameter_input.min, parameter_input.max)
+            elif parameter_input.prior == 'beta':
+                prior = pm.Beta(parameter, parameter_input.a, parameter_input.b)
+            else:
+                raise ValueError('Unrecognized prior')
+            priors.update({parameter: prior})
+
+        # Define the model: parameters and observed data
         if stats_model.distribution == 'betabinomial':
-            # Define the model: parameters and observed data
-            prior_alpha = pm.Uniform('alpha', stats_model.parameters.alpha.min, stats_model.parameters.alpha.max)
-            prior_beta = pm.Uniform('beta', stats_model.parameters.beta.min, stats_model.parameters.beta.max)
-            pm.BetaBinomial('k', alpha=prior_alpha, beta=prior_beta, n=scores_obs[:,1], observed=scores_obs[:,0])
-            # Do simulations and sample from the posterior distributions
-            trace = pm.sample(draws=mcmc_settings.draw_count, chains=mcmc_settings.chain_count,
-                              tune=mcmc_settings.tune_count, cores=1, random_seed=None, progressbar=False)
-            # Get the posterior samples of the model parameters, and some (convergence) statistics
-            post_alpha = np.concatenate(np.array(trace.posterior['alpha']))
-            post_beta = np.concatenate(np.array(trace.posterior['beta']))
-            # Calculate probabilities on specified score grid values
-            post_alpha_2d = np.tile(np.expand_dims(post_alpha, 0), (len(scores_eval), 1))
-            post_beta_2d = np.tile(np.expand_dims(post_beta, 0), (len(scores_eval), 1))
-            probs = betabinom.pmf(k_obs_2d, n_obs_2d, post_alpha_2d, post_beta_2d)
+            pm.BetaBinomial('k', alpha=priors['alpha'], beta=priors['beta'], n=scores_obs[:,1], observed=scores_obs[:,0])
         elif stats_model.distribution == 'binomial':
             # Define the model: parameters and observed data
-            prior_p = pm.Beta('p', stats_model.parameters.p.a, stats_model.parameters.p.b)
-            pm.Binomial('k', p=prior_p, n=np.sum(scores_obs[:,1]), observed=np.sum(scores_obs[:,0]))
-            # Do simulations and sample from the posterior distributions
-            trace = pm.sample(draws=mcmc_settings.draw_count, chains=mcmc_settings.chain_count,
-                              tune=mcmc_settings.tune_count, cores=1, random_seed=None, progressbar=False)
-            # Get the posterior samples of the model parameters, and some (convergence) statistics
-            post_p = np.concatenate(np.array(trace.posterior['p']))
+            pm.Binomial('k', p=priors['p'], n=np.sum(scores_obs[:,1]), observed=np.sum(scores_obs[:,0]))
+        else:
+            raise ValueError('Unrecognized distribution')
+
+        # Do simulations and sample from the posterior distributions
+        trace = pm.sample(draws=mcmc_settings.draw_count, chains=mcmc_settings.chain_count,
+                          tune=mcmc_settings.tune_count, cores=1, random_seed=None, progressbar=False)
+
+        # Get the posterior samples of the model parameters, and print some (convergence) statistics
+        parameter_samples = {}
+        for parameter in parameters:
+            samples = np.concatenate(np.array(trace.posterior[parameter]))
+            parameter_samples.update({parameter: samples})
+        summary = az.summary(trace, round_to=6)
+        print(summary[['mean', 'sd', 'r_hat']])
+
+    return parameter_samples
+
+
+def apply_mcmc(stats_model, parameters, scores_eval):
+
+    # Prepare parameter and scores for 2d-evaluations
+    sample_count = len(parameters[list(parameters.keys())[0]])
+    scores_2d = {}
+    for score_id in range(scores_eval.shape[1]):
+        score_2d = np.tile(np.expand_dims(scores_eval[:,score_id], 1), (1, sample_count))
+        scores_2d.update({score_id: score_2d})
+    parameters_2d = {}
+    for parameter in list(parameters.keys()):
+        parameter_2d = np.tile(np.expand_dims(parameters[parameter], 0), (len(scores_eval), 1))
+        parameters_2d.update({parameter: parameter_2d})
+
+    # Calculate e-base log probabilities on specified scores values
+    with pm.Model():
+        if stats_model.distribution == 'betabinomial':
+            logp = betabinom.logpmf(scores_2d[0], scores_2d[1], parameters_2d['alpha'], parameters_2d['beta'])
+        elif stats_model.distribution == 'binomial':
             # Calculate probabilities on specified score grid values
-            post_p_2d = np.tile(np.expand_dims(post_p, 0), (len(scores_eval), 1))
-            probs = binom.pmf(k_obs_2d, n_obs_2d, post_p_2d)
+            logp = binom.logpmf(scores_2d[0], scores_2d[1], parameters_2d['p'])
 
-    summary = az.summary(trace, round_to=6)
-    print(summary[['mean', 'sd', 'r_hat']])
-
-    return probs
+    # Return 10-bas log probabilities
+    return logp / np.log(10)
--- lrmodule/mcmc/test_mcmc.py
+++ lrmodule/mcmc/test_mcmc.py
@@ -2,50 +2,79 @@ import confidence
 import numpy as np
 import pandas as pd
 
-from lrmodule.mcmc.lr_analyses import analyse_bayesian, run_mcmc_sims
+from lrmodule.mcmc.lr_analyses import analyse_bayesian, fit_mcmc, apply_mcmc
 
 cfg = confidence.load_name("lrmodule/mcmc/config")
 
-def test_mcmc_binomial():
 
-    csv_prefix = 'lrmodule/mcmc/firing_pin_impression-' + cfg.dataset.score
+# def test_fit_mcmc_parameter_stats():
+#
+#     csv_prefix = 'lrmodule/mcmc/firing_pin_impression-' + cfg.dataset.score
+#     df_scores = pd.read_csv(csv_prefix + '-all-input.csv', header=None)
+#
+#     # beta-binomial model
+#     scores_km = np.int32(np.array(df_scores[df_scores[0]=='km'])[:,1:])
+#     parameters_km = fit_mcmc(cfg.dataset.statistical_model.km, scores_km, cfg.dataset.statistical_model.mcmc_settings)
+#     stats_calc = np.array([[np.mean(parameters_km['alpha']), np.std(parameters_km['alpha'])],
+#                            [np.mean(parameters_km['beta']), np.std(parameters_km['beta'])]])
+#     stats_ref = np.loadtxt(csv_prefix + '-km_parameter_stats.csv', delimiter=',')
+#     assert np.allclose(stats_calc, stats_ref, rtol=1E-2, atol=1E-2)
+#
+#     # binomial model
+#     scores_knm = np.int32(np.array(df_scores[df_scores[0]=='knm'])[:,1:])
+#     parameters_knm = fit_mcmc(cfg.dataset.statistical_model.knm, scores_knm, cfg.dataset.statistical_model.mcmc_settings)
+#     stats_calc = np.array([np.mean(parameters_knm['p']), np.std(parameters_knm['p'])])
+#     stats_ref = np.loadtxt(csv_prefix + '-knm_parameter_stats.csv', delimiter=',')
+#     assert np.allclose(stats_calc, stats_ref, rtol=1E-2, atol=1E-2)
 
+def test_apply_mcmc_binomial():
+
+    csv_prefix = 'lrmodule/mcmc/firing_pin_impression-' + cfg.dataset.score#
     df_scores = pd.read_csv(csv_prefix + '-all-input.csv', header=None)
     scores_eval = np.int32(np.array(df_scores[df_scores[0]=='eval'])[:,1:])
-    scores_knm = np.int32(np.array(df_scores[df_scores[0]=='knm'])[:,1:])
-    probs_eval = run_mcmc_sims(scores_eval, scores_knm, cfg.dataset.statistical_model.knm, cfg.dataset.statistical_model.mcmc_settings)
-    probs = np.quantile(probs_eval, [cfg.interval.lower_bound, 0.5, cfg.interval.upper_bound], axis=1)
 
-    df_probs = pd.read_csv(csv_prefix + '-knm_logp.csv', header=None)
-    assert np.allclose(np.log10(probs), df_probs.to_numpy(), rtol=1E-1, atol=1E-1)
+    # betabinomial model
+    parameters_km = {'alpha': np.loadtxt(csv_prefix + '-km_parameters.csv', delimiter=',', max_rows=1),
+                     'beta': np.loadtxt(csv_prefix + '-km_parameters.csv', delimiter=',', skiprows=1)}
+    logp_eval = apply_mcmc(cfg.dataset.statistical_model.km, parameters_km, scores_eval)
+    logp_calc = np.quantile(logp_eval, [cfg.interval.lower_bound, 0.5, cfg.interval.upper_bound], axis=1)
+    logp_ref = np.loadtxt(csv_prefix + '-km_logp.csv', delimiter=',')
+    assert np.allclose(logp_calc, logp_ref, rtol=1E-2, atol=1E-2)
 
-def test_llr_firing_pin():
+    # binomial model
+    parameters_knm = {'p': np.loadtxt(csv_prefix + '-knm_parameters.csv', delimiter=',')}
+    logp_eval = apply_mcmc(cfg.dataset.statistical_model.knm, parameters_knm, scores_eval)
+    logp_calc = np.quantile(logp_eval, [cfg.interval.lower_bound, 0.5, cfg.interval.upper_bound], axis=1)
+    logp_ref = np.loadtxt(csv_prefix + '-knm_logp.csv', delimiter=',')
+    assert np.allclose(logp_calc, logp_ref, rtol=1E-2, atol=1E-2)
 
-    csv_prefix = 'lrmodule/mcmc/firing_pin_impression-' + cfg.dataset.score
-
-    df_scores = pd.read_csv(csv_prefix + '-all-input.csv', header=None)
-    scores_eval = np.int32(np.array(df_scores[df_scores[0]=='eval'])[:,1:])
-    scores_km = np.int32(np.array(df_scores[df_scores[0]=='km'])[:,1:])
-    scores_knm = np.int32(np.array(df_scores[df_scores[0]=='knm'])[:,1:])
-    llrs_eval, probs_km_eval, probs_knm_eval = analyse_bayesian(scores_eval, scores_km, scores_knm, cfg.dataset.statistical_model)
-    llrs = np.quantile(llrs_eval, [cfg.interval.lower_bound, 0.5, cfg.interval.upper_bound], axis=1)
-
-    df_llrs = pd.read_csv(csv_prefix + '-llrs-unbound.csv', header=None)
-    assert np.allclose(llrs, df_llrs.to_numpy(), atol=1E-2)
-
-
-def test_llr_breech_face():
-
-    csv_prefix = 'lrmodule/mcmc/breech_face_impression-' + cfg.dataset.score
-
-    df_scores = pd.read_csv(csv_prefix + '-all-input.csv', header=None)
-    scores_eval = np.int32(np.array(df_scores[df_scores[0]=='eval'])[:,1:])
-    scores_km = np.int32(np.array(df_scores[df_scores[0]=='km'])[:,1:])
-    scores_knm = np.int32(np.array(df_scores[df_scores[0]=='knm'])[:,1:])
-    llrs_eval, probs_km_eval, probs_knm_eval = analyse_bayesian(scores_eval, scores_km, scores_knm, cfg.dataset.statistical_model)
-    llrs = np.quantile(llrs_eval, [cfg.interval.lower_bound, 0.5, cfg.interval.upper_bound], axis=1)
-
-    df_llrs = pd.read_csv(csv_prefix + '-llrs-unbound.csv', header=None)
-    assert np.allclose(llrs, df_llrs.to_numpy(), atol=1E-2)
+# def test_llr_firing_pin():
+#
+#     csv_prefix = 'lrmodule/mcmc/firing_pin_impression-' + cfg.dataset.score
+#
+#     df_scores = pd.read_csv(csv_prefix + '-all-input.csv', header=None)
+#     scores_eval = np.int32(np.array(df_scores[df_scores[0]=='eval'])[:,1:])
+#     scores_km = np.int32(np.array(df_scores[df_scores[0]=='km'])[:,1:])
+#     scores_knm = np.int32(np.array(df_scores[df_scores[0]=='knm'])[:,1:])
+#     llrs_eval, probs_km_eval, probs_knm_eval = analyse_bayesian(scores_eval, scores_km, scores_knm, cfg.dataset.statistical_model)
+#     llrs = np.quantile(llrs_eval, [cfg.interval.lower_bound, 0.5, cfg.interval.upper_bound], axis=1)
+#
+#     df_llrs = pd.read_csv(csv_prefix + '-llr_unbound.csv', header=None)
+#     assert np.allclose(llrs, df_llrs.to_numpy(), rtol=1E-1, atol=1E-1)
+#
+#
+# def test_llr_breech_face():
+#
+#     csv_prefix = 'lrmodule/mcmc/breech_face_impression-' + cfg.dataset.score
+#
+#     df_scores = pd.read_csv(csv_prefix + '-all-input.csv', header=None)
+#     scores_eval = np.int32(np.array(df_scores[df_scores[0]=='eval'])[:,1:])
+#     scores_km = np.int32(np.array(df_scores[df_scores[0]=='km'])[:,1:])
+#     scores_knm = np.int32(np.array(df_scores[df_scores[0]=='knm'])[:,1:])
+#     llrs_eval, probs_km_eval, probs_knm_eval = analyse_bayesian(scores_eval, scores_km, scores_knm, cfg.dataset.statistical_model)
+#     llrs = np.quantile(llrs_eval, [cfg.interval.lower_bound, 0.5, cfg.interval.upper_bound], axis=1)
+#
+#     df_llrs = pd.read_csv(csv_prefix + '-llr_unbound.csv', header=None)
+#     assert np.allclose(llrs, df_llrs.to_numpy(), rtol=1E-1, atol=1E-1)
 
 print('finished')
