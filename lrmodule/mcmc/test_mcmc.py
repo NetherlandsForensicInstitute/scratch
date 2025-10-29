8f2af8914994316963cc1d9b22fd148d512f9cf8
Leen van der Ham <l.van.der.ham@nfi.nl>
2025-10-29 11:03:19 +0100

    All tests are passing now.

--- lrmodule/mcmc/test_mcmc.py
+++ lrmodule/mcmc/test_mcmc.py
@@ -2,79 +2,90 @@ import confidence
 import numpy as np
 import pandas as pd
 
-from lrmodule.mcmc.lr_analyses import analyse_bayesian, fit_mcmc, apply_mcmc
+from lrmodule.mcmc.lr_analyses import fit_mcmc, apply_mcmc, analyse_bayesian
+
+
+# Tests against references from the Matlab implementation.
 
 cfg = confidence.load_name("lrmodule/mcmc/config")
+quantages = [cfg.interval.lower_bound, 0.5, cfg.interval.upper_bound]
 
 
-# def test_fit_mcmc_parameter_stats():
-#
-#     csv_prefix = 'lrmodule/mcmc/firing_pin_impression-' + cfg.dataset.score
-#     df_scores = pd.read_csv(csv_prefix + '-all-input.csv', header=None)
-#
-#     # beta-binomial model
-#     scores_km = np.int32(np.array(df_scores[df_scores[0]=='km'])[:,1:])
-#     parameters_km = fit_mcmc(cfg.dataset.statistical_model.km, scores_km, cfg.dataset.statistical_model.mcmc_settings)
-#     stats_calc = np.array([[np.mean(parameters_km['alpha']), np.std(parameters_km['alpha'])],
-#                            [np.mean(parameters_km['beta']), np.std(parameters_km['beta'])]])
-#     stats_ref = np.loadtxt(csv_prefix + '-km_parameter_stats.csv', delimiter=',')
-#     assert np.allclose(stats_calc, stats_ref, rtol=1E-2, atol=1E-2)
-#
-#     # binomial model
-#     scores_knm = np.int32(np.array(df_scores[df_scores[0]=='knm'])[:,1:])
-#     parameters_knm = fit_mcmc(cfg.dataset.statistical_model.knm, scores_knm, cfg.dataset.statistical_model.mcmc_settings)
-#     stats_calc = np.array([np.mean(parameters_knm['p']), np.std(parameters_knm['p'])])
-#     stats_ref = np.loadtxt(csv_prefix + '-knm_parameter_stats.csv', delimiter=',')
-#     assert np.allclose(stats_calc, stats_ref, rtol=1E-2, atol=1E-2)
+def test_fit_mcmc_parameter_stats():
+
+    # Conversion from observed score data to (statistics of) model parameter samples.
+    # We expect some differences here due to mcmc-differences and randomness.
+
+    csv_prefix = 'lrmodule/mcmc/firing_pin_impression-' + cfg.dataset.score
+    df_scores = pd.read_csv(csv_prefix + '-all-input.csv', header=None)
+
+    # beta-binomial model
+    scores_km = np.int32(np.array(df_scores[df_scores[0]=='km'])[:,1:])
+    parameters_km = fit_mcmc(cfg.dataset.statistical_model.km, scores_km, cfg.dataset.statistical_model.mcmc_settings)
+    stats_calc = np.array([[np.mean(parameters_km['alpha']), np.std(parameters_km['alpha'])],
+                           [np.mean(parameters_km['beta']), np.std(parameters_km['beta'])]])
+    stats_ref = np.loadtxt(csv_prefix + '-km_parameter_stats.csv', delimiter=',')
+    assert np.allclose(stats_calc, stats_ref, rtol=2E-2, atol=2E-2)
+
+    # binomial model
+    scores_knm = np.int32(np.array(df_scores[df_scores[0]=='knm'])[:,1:])
+    parameters_knm = fit_mcmc(cfg.dataset.statistical_model.knm, scores_knm, cfg.dataset.statistical_model.mcmc_settings)
+    stats_calc = np.array([np.mean(parameters_knm['p']), np.std(parameters_knm['p'])])
+    stats_ref = np.loadtxt(csv_prefix + '-knm_parameter_stats.csv', delimiter=',')
+    assert np.allclose(stats_calc, stats_ref, rtol=1E-4, atol=1E-4)
 
 def test_apply_mcmc_binomial():
+
+    # Conversion from model parameter samples to (percentiles of) log10-probabilities.
 
     csv_prefix = 'lrmodule/mcmc/firing_pin_impression-' + cfg.dataset.score#
     df_scores = pd.read_csv(csv_prefix + '-all-input.csv', header=None)
     scores_eval = np.int32(np.array(df_scores[df_scores[0]=='eval'])[:,1:])
 
     # betabinomial model
-    parameters_km = {'alpha': np.loadtxt(csv_prefix + '-km_parameters.csv', delimiter=',', max_rows=1),
-                     'beta': np.loadtxt(csv_prefix + '-km_parameters.csv', delimiter=',', skiprows=1)}
+    alpha, beta = np.loadtxt(csv_prefix + '-km_parameter_samples.csv', delimiter=',', unpack=True)
+    parameters_km = {'alpha': alpha, 'beta': beta}
     logp_eval = apply_mcmc(cfg.dataset.statistical_model.km, parameters_km, scores_eval)
-    logp_calc = np.quantile(logp_eval, [cfg.interval.lower_bound, 0.5, cfg.interval.upper_bound], axis=1)
+    logp_calc = np.quantile(logp_eval, quantages, axis=1, method='midpoint')
     logp_ref = np.loadtxt(csv_prefix + '-km_logp.csv', delimiter=',')
-    assert np.allclose(logp_calc, logp_ref, rtol=1E-2, atol=1E-2)
+    assert np.allclose(logp_calc, logp_ref)
 
     # binomial model
-    parameters_knm = {'p': np.loadtxt(csv_prefix + '-knm_parameters.csv', delimiter=',')}
+    parameters_knm = {'p': np.loadtxt(csv_prefix + '-knm_parameter_samples.csv', delimiter=',')}
     logp_eval = apply_mcmc(cfg.dataset.statistical_model.knm, parameters_knm, scores_eval)
-    logp_calc = np.quantile(logp_eval, [cfg.interval.lower_bound, 0.5, cfg.interval.upper_bound], axis=1)
+    logp_calc = np.quantile(logp_eval, quantages, axis=1, method='midpoint')
     logp_ref = np.loadtxt(csv_prefix + '-knm_logp.csv', delimiter=',')
-    assert np.allclose(logp_calc, logp_ref, rtol=1E-2, atol=1E-2)
+    assert np.allclose(logp_calc, logp_ref)
 
-# def test_llr_firing_pin():
-#
-#     csv_prefix = 'lrmodule/mcmc/firing_pin_impression-' + cfg.dataset.score
-#
-#     df_scores = pd.read_csv(csv_prefix + '-all-input.csv', header=None)
-#     scores_eval = np.int32(np.array(df_scores[df_scores[0]=='eval'])[:,1:])
-#     scores_km = np.int32(np.array(df_scores[df_scores[0]=='km'])[:,1:])
-#     scores_knm = np.int32(np.array(df_scores[df_scores[0]=='knm'])[:,1:])
-#     llrs_eval, probs_km_eval, probs_knm_eval = analyse_bayesian(scores_eval, scores_km, scores_knm, cfg.dataset.statistical_model)
-#     llrs = np.quantile(llrs_eval, [cfg.interval.lower_bound, 0.5, cfg.interval.upper_bound], axis=1)
-#
-#     df_llrs = pd.read_csv(csv_prefix + '-llr_unbound.csv', header=None)
-#     assert np.allclose(llrs, df_llrs.to_numpy(), rtol=1E-1, atol=1E-1)
-#
-#
-# def test_llr_breech_face():
-#
-#     csv_prefix = 'lrmodule/mcmc/breech_face_impression-' + cfg.dataset.score
-#
-#     df_scores = pd.read_csv(csv_prefix + '-all-input.csv', header=None)
-#     scores_eval = np.int32(np.array(df_scores[df_scores[0]=='eval'])[:,1:])
-#     scores_km = np.int32(np.array(df_scores[df_scores[0]=='km'])[:,1:])
-#     scores_knm = np.int32(np.array(df_scores[df_scores[0]=='knm'])[:,1:])
-#     llrs_eval, probs_km_eval, probs_knm_eval = analyse_bayesian(scores_eval, scores_km, scores_knm, cfg.dataset.statistical_model)
-#     llrs = np.quantile(llrs_eval, [cfg.interval.lower_bound, 0.5, cfg.interval.upper_bound], axis=1)
-#
-#     df_llrs = pd.read_csv(csv_prefix + '-llr_unbound.csv', header=None)
-#     assert np.allclose(llrs, df_llrs.to_numpy(), rtol=1E-1, atol=1E-1)
+def test_llr_firing_pin():
 
-print('finished')
+    # From pre-calculated scores to (percentiles of) log-10 lrs.
+    # We expect some differences here due to mcmc-differences and randomness.
+
+    csv_prefix = 'lrmodule/mcmc/firing_pin_impression-' + cfg.dataset.score
+    df_scores = pd.read_csv(csv_prefix + '-all-input.csv', header=None)
+
+    scores_eval = np.int32(np.array(df_scores[df_scores[0]=='eval'])[:,1:])
+    scores_km = np.int32(np.array(df_scores[df_scores[0]=='km'])[:,1:])
+    scores_knm = np.int32(np.array(df_scores[df_scores[0]=='knm'])[:,1:])
+    logp_km, logp_knm = analyse_bayesian(scores_eval, scores_km, scores_knm, cfg.dataset.statistical_model)
+    llrs_calc = np.quantile(logp_km - logp_knm, quantages, axis=1, method='midpoint')
+    llrs_ref = np.loadtxt(csv_prefix + '-llr_unbound.csv', delimiter=',')
+    assert np.allclose(llrs_calc, llrs_ref, rtol=5E-2, atol=5E-2)
+
+
+def test_llr_breech_face():
+
+    # From pre-calculated scores to (percentiles of) log-10 lrs.
+    # We expect some differences here due to mcmc-differences and randomness.
+
+    csv_prefix = 'lrmodule/mcmc/breech_face_impression-' + cfg.dataset.score
+    df_scores = pd.read_csv(csv_prefix + '-all-input.csv', header=None)
+
+    scores_eval = np.int32(np.array(df_scores[df_scores[0]=='eval'])[:,1:])
+    scores_km = np.int32(np.array(df_scores[df_scores[0]=='km'])[:,1:])
+    scores_knm = np.int32(np.array(df_scores[df_scores[0]=='knm'])[:,1:])
+    logp_km, logp_knm = analyse_bayesian(scores_eval, scores_km, scores_knm, cfg.dataset.statistical_model)
+    llrs_calc = np.quantile(logp_km - logp_knm, quantages, axis=1, method='midpoint')
+    llrs_ref = np.loadtxt(csv_prefix + '-llr_unbound.csv', delimiter=',')
+    assert np.allclose(llrs_calc, llrs_ref, rtol=5E-2, atol=5E-2)
