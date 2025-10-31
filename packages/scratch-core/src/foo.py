d7263658ac2b6d619c476ffe8721cc600d56d49b
Sharlon N. Regales <sharlonregales@gmail.com>
2025-10-31 10:10:15 +0100

    docstring fix

--- packages/scratch-core/src/foo.py
+++ packages/scratch-core/src/foo.py
@@ -1,10 +1,19 @@
 def foo(x: str) -> str:
-    """Prints out bar followed with x.
+    """Return a formatted string prefixed with 'bar'.
 
-    Args:
-      x: str any word or sentence
+    Parameters
+    ----------
+    x : str
+        Input string to be prefixed.
 
-    Returns:
-      string pre-pend with bar
+    Returns
+    -------
+    str
+        The formatted string in the form ``"bar <x>"``.
+
+    Examples
+    --------
+    >>> foo("test")
+    'bar test'
     """
     return f"bar {x}"
