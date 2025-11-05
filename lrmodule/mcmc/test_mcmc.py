c7e3a29d2c20ad6e63d16f021af369f6270afe37
Leen van der Ham <l.van.der.ham@nfi.nl>
2025-11-05 16:18:38 +0100

    Redistributed inputs over the classes and functions; added typing.

--- lrmodule/mcmc/test_mcmc.py
+++ lrmodule/mcmc/test_mcmc.py
@@ -9,10 +9,9 @@ from lrmodule.mcmc.lr_analyses import McmcModel, McmcLLRModel
 # Tests against references from the Matlab implementation.
 
 cfg = confidence.load_name("lrmodule/mcmc/config")
-quantages = [cfg.interval.lower_bound, 0.5, cfg.interval.upper_bound]
 
 
-def test_fit_mcmc_parameter_stats():
+def test_fit_parameter_stats():
 
     # Conversion from observed score data to (statistics of) model parameter samples.
     # We expect some differences here due to mcmc-differences and randomness.
@@ -21,21 +20,21 @@ def test_fit_mcmc_parameter_stats():
     df_scores = pd.read_csv(csv_prefix + '-all-input.csv', header=None)
 
     # beta-binomial model
-    scores_km = np.int32(np.array(df_scores[df_scores[0]=='km'])[:,1:])
-    model_km = McmcModel(cfg.dataset.statistical_model.km, cfg.dataset.statistical_model.mcmc_settings).fit(scores_km)
-    stats_calc = np.array([[np.mean(model_km.parameter_samples['alpha']), np.std(model_km.parameter_samples['alpha'])],
-                           [np.mean(model_km.parameter_samples['beta']), np.std(model_km.parameter_samples['beta'])]])
+    scores_h1 = np.int32(np.array(df_scores[df_scores[0]=='km'])[:,1:])
+    model_h1 = McmcModel(cfg.distribution_h1, cfg.parameters_h1).fit(scores_h1)
+    stats_calc = np.array([[np.mean(model_h1.parameter_samples['alpha']), np.std(model_h1.parameter_samples['alpha'])],
+                           [np.mean(model_h1.parameter_samples['beta']), np.std(model_h1.parameter_samples['beta'])]])
     stats_ref = np.loadtxt(csv_prefix + '-km_parameter_stats.csv', delimiter=',')
     assert np.allclose(stats_calc, stats_ref, rtol=2E-2, atol=2E-2)
 
     # binomial model
-    scores_knm = np.int32(np.array(df_scores[df_scores[0]=='knm'])[:,1:])
-    model_knm = McmcModel(cfg.dataset.statistical_model.knm, cfg.dataset.statistical_model.mcmc_settings).fit(scores_knm)
-    stats_calc = np.array([np.mean(model_knm.parameter_samples['p']), np.std(model_knm.parameter_samples['p'])])
+    scores_h2 = np.int32(np.array(df_scores[df_scores[0]=='knm'])[:,1:])
+    model_h2 = McmcModel(cfg.distribution_h2, cfg.parameters_h2).fit(scores_h2)
+    stats_calc = np.array([np.mean(model_h2.parameter_samples['p']), np.std(model_h2.parameter_samples['p'])])
     stats_ref = np.loadtxt(csv_prefix + '-knm_parameter_stats.csv', delimiter=',')
     assert np.allclose(stats_calc, stats_ref, rtol=1E-4, atol=1E-4)
 
-def test_transform_mcmc_binomial():
+def test_transform_quantiles():
 
     # Conversion from model parameter samples to (percentiles of) log10-probabilities.
 
@@ -45,18 +44,18 @@ def test_transform_mcmc_binomial():
 
     # betabinomial model
     alpha, beta = np.loadtxt(csv_prefix + '-km_parameter_samples.csv', delimiter=',', unpack=True)
-    model_km = McmcModel(cfg.dataset.statistical_model.km, None)
-    model_km.parameter_samples = {'alpha': alpha, 'beta': beta}
-    logp_eval = model_km.transform(scores_eval)
-    logp_calc = np.quantile(logp_eval, quantages, axis=1, method='midpoint')
+    model_h1 = McmcModel(cfg.distribution_h1, None)
+    model_h1.parameter_samples = {'alpha': alpha, 'beta': beta}
+    logp_eval = model_h1.transform(scores_eval)
+    logp_calc = np.quantile(logp_eval, [0.05, 0.5, 0.95], axis=1, method='midpoint')
     logp_ref = np.loadtxt(csv_prefix + '-km_logp.csv', delimiter=',')
     assert np.allclose(logp_calc, logp_ref)
 
     # binomial model
-    model_knm = McmcModel(cfg.dataset.statistical_model.knm, None)
-    model_knm.parameter_samples = {'p': np.loadtxt(csv_prefix + '-knm_parameter_samples.csv', delimiter=',')}
-    logp_eval = model_knm.transform(scores_eval)
-    logp_calc = np.quantile(logp_eval, quantages, axis=1, method='midpoint')
+    model_h2 = McmcModel(cfg.distribution_h2, None)
+    model_h2.parameter_samples = {'p': np.loadtxt(csv_prefix + '-knm_parameter_samples.csv', delimiter=',')}
+    logp_eval = model_h2.transform(scores_eval)
+    logp_calc = np.quantile(logp_eval, [0.05, 0.5, 0.95], axis=1, method='midpoint')
     logp_ref = np.loadtxt(csv_prefix + '-knm_logp.csv', delimiter=',')
     assert np.allclose(logp_calc, logp_ref)
 
@@ -80,7 +79,7 @@ def test_llr_dataset(dataset_name: str):
     features = np.concatenate([scores_km, scores_knm])
     labels = np.concatenate([np.ones(scores_km.shape[0]), np.zeros(scores_knm.shape[0])])
 
-    model = McmcLLRModel(pm.Uniform(3, 1, 2), cfg.dataset.statistical_model.mcmc_settings, (cfg.interval.lower_bound, cfg.interval.upper_bound))
+    model = McmcLLRModel(cfg.distribution_h1, cfg.parameters_h1, cfg.distribution_h2, cfg.parameters_h2)
     model.fit(FeatureData(features=features, labels=labels))
     llrs = model.transform(FeatureData(features=scores_eval))
     llrs_ref = np.loadtxt(csv_prefix + '-llr_unbound.csv', delimiter=',')
