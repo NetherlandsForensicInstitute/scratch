78100e1c4c35e730ea2e70f545650e274ecf8fa4
wauter <20113294+wowtor@users.noreply.github.com>
2025-11-05 11:15:15 +0100

    move fit() and transform() to McmcModel class

--- lrmodule/mcmc/test_mcmc.py
+++ lrmodule/mcmc/test_mcmc.py
@@ -2,8 +2,7 @@ import confidence
 import numpy as np
 import pandas as pd
 
-from lrmodule.mcmc.lr_analyses import fit_mcmc, apply_mcmc, analyse_bayesian
-
+from lrmodule.mcmc.lr_analyses import analyse_bayesian, McmcModel
 
 # Tests against references from the Matlab implementation.
 
@@ -21,20 +20,20 @@ def test_fit_mcmc_parameter_stats():
 
     # beta-binomial model
     scores_km = np.int32(np.array(df_scores[df_scores[0]=='km'])[:,1:])
-    parameters_km = fit_mcmc(cfg.dataset.statistical_model.km, scores_km, cfg.dataset.statistical_model.mcmc_settings)
-    stats_calc = np.array([[np.mean(parameters_km['alpha']), np.std(parameters_km['alpha'])],
-                           [np.mean(parameters_km['beta']), np.std(parameters_km['beta'])]])
+    model_km = McmcModel(cfg.dataset.statistical_model.km, cfg.dataset.statistical_model.mcmc_settings).fit(scores_km)
+    stats_calc = np.array([[np.mean(model_km.parameter_samples['alpha']), np.std(model_km.parameter_samples['alpha'])],
+                           [np.mean(model_km.parameter_samples['beta']), np.std(model_km.parameter_samples['beta'])]])
     stats_ref = np.loadtxt(csv_prefix + '-km_parameter_stats.csv', delimiter=',')
     assert np.allclose(stats_calc, stats_ref, rtol=2E-2, atol=2E-2)
 
     # binomial model
     scores_knm = np.int32(np.array(df_scores[df_scores[0]=='knm'])[:,1:])
-    parameters_knm = fit_mcmc(cfg.dataset.statistical_model.knm, scores_knm, cfg.dataset.statistical_model.mcmc_settings)
-    stats_calc = np.array([np.mean(parameters_knm['p']), np.std(parameters_knm['p'])])
+    model_knm = McmcModel(cfg.dataset.statistical_model.knm, cfg.dataset.statistical_model.mcmc_settings).fit(scores_knm)
+    stats_calc = np.array([np.mean(model_knm.parameter_samples['p']), np.std(model_knm.parameter_samples['p'])])
     stats_ref = np.loadtxt(csv_prefix + '-knm_parameter_stats.csv', delimiter=',')
     assert np.allclose(stats_calc, stats_ref, rtol=1E-4, atol=1E-4)
 
-def test_apply_mcmc_binomial():
+def test_transform_mcmc_binomial():
 
     # Conversion from model parameter samples to (percentiles of) log10-probabilities.
 
@@ -44,15 +43,17 @@ def test_apply_mcmc_binomial():
 
     # betabinomial model
     alpha, beta = np.loadtxt(csv_prefix + '-km_parameter_samples.csv', delimiter=',', unpack=True)
-    parameters_km = {'alpha': alpha, 'beta': beta}
-    logp_eval = apply_mcmc(cfg.dataset.statistical_model.km, parameters_km, scores_eval)
+    model_km = McmcModel(cfg.dataset.statistical_model.km, None)
+    model_km.parameter_samples = {'alpha': alpha, 'beta': beta}
+    logp_eval = model_km.transform(scores_eval)
     logp_calc = np.quantile(logp_eval, quantages, axis=1, method='midpoint')
     logp_ref = np.loadtxt(csv_prefix + '-km_logp.csv', delimiter=',')
     assert np.allclose(logp_calc, logp_ref)
 
     # binomial model
-    parameters_knm = {'p': np.loadtxt(csv_prefix + '-knm_parameter_samples.csv', delimiter=',')}
-    logp_eval = apply_mcmc(cfg.dataset.statistical_model.knm, parameters_knm, scores_eval)
+    model_knm = McmcModel(cfg.dataset.statistical_model.knm, None)
+    model_knm.parameter_samples = {'p': np.loadtxt(csv_prefix + '-knm_parameter_samples.csv', delimiter=',')}
+    logp_eval = model_knm.transform(scores_eval)
     logp_calc = np.quantile(logp_eval, quantages, axis=1, method='midpoint')
     logp_ref = np.loadtxt(csv_prefix + '-knm_logp.csv', delimiter=',')
     assert np.allclose(logp_calc, logp_ref)
