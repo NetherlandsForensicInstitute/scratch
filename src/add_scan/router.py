0ac24ce318c7e8a6b7f2e8ae7c86474db1226c69
Sharlon N. Regales <sharlonregales@gmail.com>
2025-11-20 12:40:31 +0100

    initial add-scan enpoint

---
+++ src/add_scan/router.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter
+
+
+class ParseError(Exception): ...
+
+
+pre_processors = APIRouter(
+    prefix="/add-scan",
+    tags=["add-scan"],
+)
+
+
+@pre_processors.post(
+    path="/",
+    summary="Add a scan file to be processed",
+    description="""""",
+)
+async def add_scan_root() -> dict[str, str]:
+    """TODO."""
+    # parse parse incoming file
+    # raise Unable to ParseError
+    # subsample the parsed file
+    # export newly created files to output directory
+    # create surface map png
+    # export png to output directory
+    return {"message": "Hello from add-scan"}
