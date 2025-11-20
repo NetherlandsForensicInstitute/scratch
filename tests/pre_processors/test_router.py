9dedd7c93fb3a4514a60db2a5abd00523611f846
Sharlon N. Regales <sharlonregales@gmail.com>
2025-11-20 17:56:49 +0100

    fix post request test

--- tests/pre_processors/test_router.py
+++ tests/pre_processors/test_router.py
@@ -1,10 +1,25 @@
+from pathlib import Path
+
 from fastapi.testclient import TestClient
 from starlette.status import HTTP_200_OK
 
+from constants import PROJECT_ROOT
 
-def test_add_scan_placeholder(client: TestClient) -> None:
+
+def test_add_scan_placeholder(client: TestClient, tmp_path: Path) -> None:
+    # Arrange, We might want to pull resources out to a more global space
+    scan_file = PROJECT_ROOT / "packages/scratch-core/tests/resources/scans/circle.al3d"
+    output_dir = tmp_path / "output"
+    output_dir.mkdir()  # Create output directory
+
     # Act
-    response = client.post("/pre-processor/add-scan/")
+    response = client.post(
+        "/pre-processor/add-scan/",
+        json={
+            "scan_file": str(scan_file),
+            "output_dir": str(output_dir),
+        },
+    )
 
     # Assert
     assert response.status_code == HTTP_200_OK, "endpoint is alive"
