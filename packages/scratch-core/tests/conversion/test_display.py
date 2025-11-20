c269ef934a7f3516a20cd021849729f55f67c5fa
cfs-data <145435153+cfs-data@users.noreply.github.com>
2025-11-20 15:49:03 +0100

    refined test

--- packages/scratch-core/tests/conversion/test_display.py
+++ packages/scratch-core/tests/conversion/test_display.py
@@ -5,15 +5,23 @@ import pytest
 
 
 @pytest.mark.parametrize("std_scaler", [0.5, 1, 2, 4, 8])
-def test_image_is_clipped_correctly(scan_image_with_nans: ScanImage, std_scaler: float):
+def test_image_is_clipped_correctly(
+    scan_image_with_nans: ScanImage, std_scaler: float, atol: float
+):
     data = scan_image_with_nans.data
     data_min, data_max = np.nanmin(data), np.nanmax(data)
-    clipped, lower, upper = clip_data(data, std_scaler=std_scaler)
-    clipped_data_min, clipped_data_max = np.nanmin(clipped), np.nanmax(clipped)
-    assert data_min <= clipped_data_min
-    assert lower <= clipped_data_min
-    assert data_max >= clipped_data_max
-    assert upper >= clipped_data_max
+    mean, std = np.nanmean(data), np.nanstd(data) * std_scaler
+
+    clipped, lower, upper = clip_data(data, std_scaler)
+    assert np.isclose(lower, mean - std, atol=atol)
+    assert np.isclose(upper, mean + std, atol=atol)
+
+    clipped_min, clipped_max = np.nanmin(clipped), np.nanmax(clipped)
+    assert lower <= clipped_min
+    assert upper >= clipped_max
+    assert data_min <= clipped_min
+    assert data_max >= clipped_max
+
     assert np.array_equal(np.isnan(data), np.isnan(clipped))
 
 
@@ -22,5 +30,6 @@ def test_get_image_for_display(scan_image_with_nans: ScanImage):
     assert display_image.width == scan_image_with_nans.width
     assert display_image.height == scan_image_with_nans.height
     assert display_image.mode == "RGBA"
+
     image_data = np.asarray(display_image)
     assert np.array_equal(np.isnan(scan_image_with_nans.data), image_data[..., -1] == 0)
