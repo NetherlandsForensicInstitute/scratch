0766998396a2ae7c34929f63ef1dd6ea6a9dd8f7
Raytesnel <raymondhouwing@gmail.com>
2025-11-24 12:27:08 +0100

    move all light_vectors to fixture

--- packages/scratch-core/tests/image_generation/test_translations.py
+++ packages/scratch-core/tests/image_generation/test_translations.py
@@ -10,7 +10,7 @@ from image_generation.translations import (
     normalize_intensity_map,
     apply_multiple_lights,
 )
-from utils.array_definitions import IMAGE_3_LAYER_STACK_ARRAY
+from utils.array_definitions import IMAGE_3_LAYER_STACK_ARRAY, NORMAL_VECTOR
 
 
 class TestComputeSurfaceNormals:
@@ -194,53 +194,60 @@ class TestCalculateLighting:
     TOLERANCE = 1e-5
 
     @pytest.fixture(scope="class")
+    def light_vector(self):
+        return LightAngle(azimuth=45, elevation=180).vector
+
+    @pytest.fixture(scope="class")
+    def observer_vector(self):
+        return LightAngle(azimuth=0, elevation=90).vector
+
+    @pytest.fixture(scope="class")
     def base_images(self) -> IMAGE_3_LAYER_STACK_ARRAY:
         nx = np.full((self.TEST_IMAGE_WIDTH, self.TEST_IMAGE_HEIGHT), 0.7)
         ny = np.full((self.TEST_IMAGE_WIDTH, self.TEST_IMAGE_HEIGHT), 0.6)
         nz = np.full((self.TEST_IMAGE_WIDTH, self.TEST_IMAGE_HEIGHT), 0.2)
         return np.stack([nx, ny, nz], axis=-1)
 
-    def test_shape(self, base_images):
-        # Arrange
-        light_source = LightAngle(azimuth=45, elevation=180)
-        observer_vector = LightAngle(azimuth=0, elevation=90)
-
+    def test_shape(
+        self,
+        base_images: IMAGE_3_LAYER_STACK_ARRAY,
+        observer_vector: NORMAL_VECTOR,
+        light_vector: NORMAL_VECTOR,
+    ):
         # Act
-        out = calculate_lighting(
-            light_source.vector, observer_vector.vector, base_images
-        )
+        out = calculate_lighting(light_vector, observer_vector, base_images)
 
         # Assert
         assert out.shape == (self.TEST_IMAGE_WIDTH, self.TEST_IMAGE_HEIGHT)
 
-    def test_value_range(self, base_images):
-        # Arrange
-        light_source = LightAngle(azimuth=45, elevation=180)
-        observer_vector = LightAngle(azimuth=0, elevation=90)
-
+    def test_value_range(
+        self,
+        base_images: IMAGE_3_LAYER_STACK_ARRAY,
+        observer_vector: NORMAL_VECTOR,
+        light_vector: NORMAL_VECTOR,
+    ):
         # Act
-        out = calculate_lighting(
-            light_source.vector, observer_vector.vector, base_images
-        )
+        out = calculate_lighting(light_vector, observer_vector, base_images)
 
         # Assert
         assert np.all(out >= 0)
         assert np.all(out <= 1)
 
-    def test_constant_normals_give_constant_output(self, base_images):
-        # Arrange
-        light_source = LightAngle(azimuth=10, elevation=30)
-        observer_vector = LightAngle(azimuth=0, elevation=90)
-
+    def test_constant_normals_give_constant_output(
+        self,
+        base_images: IMAGE_3_LAYER_STACK_ARRAY,
+        observer_vector: NORMAL_VECTOR,
+        light_vector: NORMAL_VECTOR,
+    ):
         # Act
-        out = calculate_lighting(
-            light_source.vector, observer_vector.vector, base_images
-        )
+        out = calculate_lighting(light_vector, observer_vector, base_images)
 
         # Assert
         assert np.allclose(out, out[0, 0])
 
-    def test_bump_changes_values(self):
+    def test_bump_changes_values(
+        self, observer_vector: NORMAL_VECTOR, light_vector: NORMAL_VECTOR
+    ):
         """Test that the shader reacts per pixel by giving a bump in the normals. and thest the location is changed"""
         # Arrange
         nx = np.zeros((self.TEST_IMAGE_WIDTH, self.TEST_IMAGE_HEIGHT))
@@ -248,13 +255,9 @@ class TestCalculateLighting:
         nz = np.ones((self.TEST_IMAGE_WIDTH, self.TEST_IMAGE_HEIGHT))
         nz[self.TEST_IMAGE_WIDTH // 2, self.TEST_IMAGE_HEIGHT // 2] = 1.3
         base_images = np.stack([nx, ny, nz], axis=-1)
-        light_source = LightAngle(azimuth=45, elevation=45)
-        observer_vector = LightAngle(azimuth=0, elevation=90)
 
         # Act
-        out = calculate_lighting(
-            light_source.vector, observer_vector.vector, base_images
-        )
+        out = calculate_lighting(light_vector, observer_vector, base_images)
 
         # Assert
         center = out[self.TEST_IMAGE_WIDTH // 2, self.TEST_IMAGE_HEIGHT // 2]
@@ -305,10 +308,11 @@ class TestCalculateLighting:
             ),
         ],
     )
-    def test_diffuse_clamps_to_zero(self, light_source, nx, ny, nz):
+    def test_diffuse_clamps_to_zero(
+        self, light_source: NORMAL_VECTOR, nx, ny, nz, observer_vector: NORMAL_VECTOR
+    ):
         """Opposite direction → diffuse should be 0."""
         # Arrange
-        observer_vector = np.array([0.0, 0.0, 1.0])
         base_images = np.stack([nx, ny, nz], axis=-1)
         # Act
         out = calculate_lighting(light_source, observer_vector, base_images)
@@ -316,30 +320,28 @@ class TestCalculateLighting:
         # Assert
         assert np.all(out == 0), "values should be 0."
 
-    def test_specular_maximum_case(self):
+    def test_specular_maximum_case(self, observer_vector: NORMAL_VECTOR):
         """If light, observer, and normal all align, specular should be maximal."""
         # Arrange
         nx = np.zeros((self.TEST_IMAGE_WIDTH, self.TEST_IMAGE_HEIGHT))
         ny = np.zeros((self.TEST_IMAGE_WIDTH, self.TEST_IMAGE_HEIGHT))
         nz = np.ones((self.TEST_IMAGE_WIDTH, self.TEST_IMAGE_HEIGHT))
         base_images = np.stack([nx, ny, nz], axis=-1)
-        light_source = np.array([0.0, 0.0, 1.0])
-        observer_vector = np.array([0.0, 0.0, 1.0])
+        light_vector = np.array([0.0, 0.0, 1.0])
 
         # Act
-        out = calculate_lighting(light_source, observer_vector, base_images)
+        out = calculate_lighting(light_vector, observer_vector, base_images)
 
         # Assert
         assert np.allclose(out, 1.0), "(diffuse=1, specular=1), output = (1+1)/2 = 1"
 
-    def test_lighting_known_value(self, base_images):
-        # Arrange
-        light_source = np.array([1.0, 0.0, 0.0])
-        observer_vector = np.array([0.0, 1.0, 0.0])
-        expected_constant = 0.46335
+    def test_lighting_known_value(
+        self, base_images, observer_vector: NORMAL_VECTOR, light_vector: NORMAL_VECTOR
+    ):
+        expected_constant = 0.03535534
 
         # Act
-        out = calculate_lighting(light_source, observer_vector, base_images)
+        out = calculate_lighting(light_vector, observer_vector, base_images)
 
         # Assert
         assert np.allclose(out, expected_constant, atol=self.TOLERANCE)
