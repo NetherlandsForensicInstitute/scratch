0050d193a44e578730a663514db50a4fcc5e73f5
Raytesnel <raymondhouwing@gmail.om>
2025-11-24 17:05:02 +0100

    update naming of dataformats

--- packages/scratch-core/src/utils/array_definitions.py
+++ packages/scratch-core/src/utils/array_definitions.py
@@ -5,9 +5,9 @@ from typing import Annotated
 HeightWidthShape = "*, *"
 HeightWidthNLayers = f"{HeightWidthShape}, *"
 HeightWidth3Layers = f"{HeightWidthShape}, 3"
-NormalVector = "3"
+UnitVector = "3"
 
 ScanMap2DArray = Annotated[NDArray[Shape[HeightWidthShape], float64], ...]  # type: ignore
 ScanTensor3DArray = Annotated[NDArray[Shape[HeightWidthNLayers], float64], ...]  # type: ignore
 ScanVectorField2DArray = Annotated[NDArray[Shape[HeightWidth3Layers], float64], ...]  # type: ignore
-Vector3DArray = Annotated[NDArray[Shape[NormalVector], float64], ...]  # type: ignore
+Vector3DArray = Annotated[NDArray[Shape[UnitVector], float64], ...]  # type: ignore
