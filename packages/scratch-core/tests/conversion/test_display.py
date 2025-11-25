8850a47f15e549baf50f53f1a52b95df5aefee30
cfs-data <145435153+cfs-data@users.noreply.github.com>
2025-11-25 13:42:36 +0100

    merge with target branch

--- packages/scratch-core/tests/conversion/test_display.py
+++ packages/scratch-core/tests/conversion/test_display.py
@@ -3,18 +3,18 @@ from parsers import ScanImage
 import numpy as np
 import pytest
 
+PRECISION = 1e-16
+
 
 @pytest.mark.parametrize("std_scaler", [0.5, 1, 2, 4, 8])
-def test_image_is_clipped_correctly(
-    scan_image_with_nans: ScanImage, std_scaler: float, atol: float
-):
+def test_image_is_clipped_correctly(scan_image_with_nans: ScanImage, std_scaler: float):
     data = scan_image_with_nans.data
     data_min, data_max = np.nanmin(data), np.nanmax(data)
     mean, std = np.nanmean(data), np.nanstd(data, ddof=1) * std_scaler
 
     clipped, lower, upper = clip_data(data, std_scaler)
-    assert np.isclose(lower, mean - std, atol=atol)
-    assert np.isclose(upper, mean + std, atol=atol)
+    assert np.isclose(lower, mean - std, atol=PRECISION)
+    assert np.isclose(upper, mean + std, atol=PRECISION)
 
     clipped_min, clipped_max = np.nanmin(clipped), np.nanmax(clipped)
     assert lower <= clipped_min
