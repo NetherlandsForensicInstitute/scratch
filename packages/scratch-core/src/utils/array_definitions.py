34f4f7b8ced5e6ff52bae2785e5c2ae7f3d7cda3
Raytesnel <raymondhouwing@gmail.om>
2025-11-25 18:17:24 +0100

    expose only array definitions we use

--- packages/scratch-core/src/utils/array_definitions.py
+++ packages/scratch-core/src/utils/array_definitions.py
@@ -11,3 +11,10 @@ ScanMap2DArray = Annotated[NDArray[Shape[HeightWidthShape], float64], ...]  # ty
 ScanTensor3DArray = Annotated[NDArray[Shape[HeightWidthNLayers], float64], ...]  # type: ignore
 ScanVectorField2DArray = Annotated[NDArray[Shape[HeightWidth3Layers], float64], ...]  # type: ignore
 Vector3DArray = Annotated[NDArray[Shape[UnitVector], float64], ...]  # type: ignore
+
+__all__ = [
+    "ScanMap2DArray",
+    "ScanTensor3DArray",
+    "ScanVectorField2DArray",
+    "Vector3DArray",
+]
