be3689b43129c9c73f939d94da829bc5023deaa7
Sharlon  N. Regales <31469087+snregales@users.noreply.github.com>
2025-11-26 15:44:16 +0000

    initial add-scan enpoint (#50)
    * Add a generic base config schema
    
    * initial add-scan enpoint
    
    * Add tests and hypothesis
    
    * remove image file support
    
    * remove some unsupported extensions
    
    * Add pydantic model to endpoint
    
    * Use agreed opon rout on the schematic drawing
    
    * Create a return type for our endpoint
    
    * added description, possible responses to the endpoint and examples to the schemas
    
    * add docstring in contract tests
    
    * Rename RootRout to RoutePrefix and update rout to route
    
    * ignore result_images on all sub directories
    
    ---------
    
    Co-authored-by: Raytesnel <raymondhouwing@gmail.om>
    Co-authored-by: Raytesnel <raymondhouwing@gmail.com>

---
+++ src/preprocessors/__init__.py
@@ -0,0 +1,8 @@
+from .router import preprocessor_route
+from .schemas import ProcessedDataLocation, UploadScan
+
+__all__ = (
+    "preprocessor_route",
+    "UploadScan",
+    "ProcessedDataLocation",
+)
---
+++ tests/preprocessors/test_router.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+
+import pytest
+from fastapi.testclient import TestClient
+from starlette.status import HTTP_200_OK
+
+from constants import PROJECT_ROOT
+from preprocessors import ProcessedDataLocation, UploadScan
+
+
+def test_pre_processors_placeholder(client: TestClient) -> None:
+    # Act
+    response = client.get("/preprocessor")
+
+    # Assert
+    assert response.status_code == HTTP_200_OK, "endpoint is alive"
+    assert response.json() == {"message": "Hello from the pre-processors"}, "A placeholder response should be returned"
+
+
+@pytest.mark.integration
+def test_proces_scan(client: TestClient, tmp_path: Path) -> None:
+    # Arrange
+    scan_file = PROJECT_ROOT / "packages/scratch-core/tests/resources/scans/circle.x3p"
+    input_model = UploadScan(
+        scan_file=scan_file,
+        output_dir=tmp_path,
+    )
+    # TODO this will make the files till we implemented the endpoint to bypass the validations
+    (input_model.output_dir / "circle.x3p").touch()
+    (input_model.output_dir / "preview.png").touch()
+    (input_model.output_dir / "surface_map.png").touch()
+
+    expected_response = ProcessedDataLocation(
+        preview_image=input_model.output_dir / "preview.png",
+        surfacemap_image=input_model.output_dir / "surface_map.png",
+        x3p_image=input_model.output_dir / "circle.x3p",
+    )
+
+    # Act
+    response = client.post("/preprocessor/process-scan", json=input_model.model_dump(mode="json"), timeout=5)
+
+    # Assert
+    assert response.status_code == HTTP_200_OK, "endpoint is alive"
+    response_model = expected_response.model_validate(response.json())
+    assert response_model == expected_response
