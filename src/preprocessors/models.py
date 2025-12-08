af7d43283d6e12cb66b49f689a94de459323add0
Raytesnel <raymondhouwing@gmail.com>
2025-12-08 21:55:11 +0100

    implement endpoint

---
+++ src/preprocessors/models.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+from pydantic.fields import Field
+
+
+class ImageGenerationError(BaseModel):
+    message: str = Field(
+        default="failed to generate preview_image.png",
+        examples=[
+            "Failed to export scan_image.x3p",
+            "Failed to export preview_image.png",
+            "Failed to export surface_map.x3p",
+        ],
+    )
+    error: str = Field(default="ZeroDivisionError: ....", examples=["ValueError: ...."])
+
+
+class ParsingError(BaseModel):
+    message: str = Field(default="failed to parse scan file", examples=["Failed to parse scan file"])
+    error: str = Field(default="ExportError: ....", examples=["FileIsCorrupted: ...."])
