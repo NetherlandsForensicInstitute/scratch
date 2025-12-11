bf5e122f60a9296c52dbc17de25e8f1c341132c7
Sharlon N. Regales <sharlonregales@gmail.com>
2025-12-11 11:20:40 +0100

    Entegrate new pipeline structure to the pipeline

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,9 @@
+from pathlib import Path
+
 import pytest
 from fastapi.testclient import TestClient
 
+from constants import PROJECT_ROOT
 from main import app
 
 
@@ -8,3 +11,8 @@ from main import app
 def client():
     with TestClient(app) as c:
         yield c
+
+
+@pytest.fixture(scope="session")
+def scan_directory() -> Path:
+    return PROJECT_ROOT / "packages/scratch-core/tests/resources/scans"
