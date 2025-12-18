ed2b6538cc505c168e331230e4161301072c2cee
cfs-data <145435153+cfs-data@users.noreply.github.com>
2025-12-18 15:59:44 +0100

    changed order

--- packages/scratch-core/src/conversion/resample.py
+++ packages/scratch-core/src/conversion/resample.py
@@ -37,10 +37,9 @@ def resample_image_and_mask(
         )
     if only_downsample:
         factors = clip_factors(factors, preserve_aspect_ratio)
-    factor_x, factor_y = factors
-
     if np.allclose(factors, 1.0):
         return scan_image, mask
+    factor_x, factor_y = factors
 
     image = resample_scan_image(scan_image, factor_x=factor_x, factor_y=factor_y)
     if mask is not None:
