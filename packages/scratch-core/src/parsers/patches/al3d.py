9a496dbd9a50a3d22b48e8438086400b50858fb6
cfs-data <145435153+cfs-data@users.noreply.github.com>
2026-01-13 15:20:50 +0100

    add comments

--- packages/scratch-core/src/parsers/patches/al3d.py
+++ packages/scratch-core/src/parsers/patches/al3d.py
@@ -41,26 +41,27 @@ def read_al3d(filehandle, read_image_layers=False, encoding="utf-8"):
     step_y = float(header["PixelSizeYMeter"]) * 1e6
     offset = int(header["DepthImageOffset"])
     filehandle.seek(offset)
-
     # data = read_array(filehandle, dtype=np.float32, count=nx * ny, offset=0).reshape(ny, nx)
-
-    # === Our Patch Start ===
-    # Compute the stride depending on whether `nx` is odd or even
-    stride = nx + (nx % 2)
-    data = read_array(filehandle, dtype=DTYPE, count=ny * stride, offset=0)
-    # Compute the image width from the parsed buffer shape
-    image_width = data.shape[0] // ny
-    data = data.reshape(ny, image_width)
-    if image_width > nx:
-        # Ensure only valid data is returned
-        data = data[:, :nx]
-    # === Our Patch End ===
 
     # invalidValue = float(header["InvalidPixelValue"])
     # data[data == invalidValue] = np.nan   # This comparison may fail for certain cases
 
     # === Our Patch Start ===
+    # Compute the correct stride depending on whether `nx` is odd or even
+    stride = nx + (nx % 2)
+    data = read_array(filehandle, dtype=DTYPE, count=ny * stride, offset=0)
+    # Compute the image width from the parsed buffer shape
+    # Note: we cannot reuse `stride` for this, since the number of values parsed from the buffer can be smaller than `ny * stride`
+    image_width = data.shape[0] // ny
+    data = data.reshape(ny, image_width)
+    # When `nx` is odd, the computed image width may exceed the tag value `nx`
+    # In that case, the values parsed from the buffer are undefined or zero's
+    if image_width > nx:
+        # Ensure only valid data is returned
+        data = data[:, :nx]
+    # Store the numerical value for invalid pixels as a float32
     sentinel = DTYPE(header["InvalidPixelValue"])
+    # Replace these values with NaN values
     data[np.isclose(data, sentinel, rtol=0.0, atol=1e-6)] = np.nan
     # === Our Patch End ===
 
