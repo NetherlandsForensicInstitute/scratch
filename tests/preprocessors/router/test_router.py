0ba4f15aba7b9575c7f4b762425a78e65ff6351a
Simone Ariens <s.ariens@nfi.nl>
2026-01-23 10:01:59 +0100

    scan_file_path as fixture

--- tests/preprocessors/router/test_router.py
+++ tests/preprocessors/router/test_router.py
@@ -76,9 +76,13 @@ class TestPrepareMarkEndpoint:
         directory_access.resource_path.mkdir(exist_ok=True)
         monkeypatch.setattr("preprocessors.router.create_vault", lambda _: directory_access)
 
+    @pytest.fixture(autouse=True)
+    def scan_file_path(self, scan_directory: Path):
+        """Path to a dummy scan image file."""
+        self.scan_file_path = scan_directory / "circle.x3p"
+
     def get_schema_for_endpoint(
         self,
-        scan_file_path: Path,
         schema: type[PrepareMarkImpression | PrepareMarkStriation],
         mark_type: str,
         mark_parameters: type[PreprocessingStriationParams | PreprocessingImpressionParams],
@@ -87,7 +91,7 @@ class TestPrepareMarkEndpoint:
         return schema(
             project_name="test_project",
             mark_type=mark_type,  # type: ignore
-            scan_file=scan_file_path,
+            scan_file=self.scan_file_path,
             mask_array=[[0, 1], [1, 0]],
             crop_info=CropInfo(type=MaskTypes.CIRCLE, data={}, is_foreground=False),
             rotation_angle=15,
@@ -96,7 +100,6 @@ class TestPrepareMarkEndpoint:
 
     def test_prepare_mark_endpoint_returns_urls(  # noqa: PLR0913
         self,
-        scan_directory: Path,
         client: TestClient,
         subroute: str,
         schema: type[PrepareMarkImpression | PrepareMarkStriation],
@@ -107,7 +110,6 @@ class TestPrepareMarkEndpoint:
         """Test that the prepare-mark endpoint processes the request and returns file URLs."""
         # Arrange
         payload = self.get_schema_for_endpoint(
-            scan_file_path=scan_directory / "circle.x3p",
             schema=schema,
             mark_type=mark_type,
             mark_parameters=mark_parameters,
@@ -125,7 +127,6 @@ class TestPrepareMarkEndpoint:
 
     def test_prepare_mark_endpoint_has_made_files_in_vault(  # noqa: PLR0913
         self,
-        scan_directory: Path,
         client: TestClient,
         directory_access: DirectoryAccess,
         schema: type[PrepareMarkImpression | PrepareMarkStriation],
@@ -137,7 +138,6 @@ class TestPrepareMarkEndpoint:
         """Test that the prepare-mark endpoint creates files in the vault."""
         # Arrange
         payload = self.get_schema_for_endpoint(
-            scan_file_path=scan_directory / "circle.x3p",
             schema=schema,  # type: ignore
             mark_type=mark_type,
             mark_parameters=mark_parameters,  # type: ignore
@@ -156,7 +156,6 @@ class TestPrepareMarkEndpoint:
 
     def test_prepare_mark_endpoint_response_url_matches_folder_location(  # noqa: PLR0913
         self,
-        scan_directory: Path,
         client: TestClient,
         directory_access: DirectoryAccess,
         schema: type[PrepareMarkImpression | PrepareMarkStriation],
@@ -168,7 +167,6 @@ class TestPrepareMarkEndpoint:
         """Test that the URLs in the prepare-mark endpoint response match the vault folder location."""
         # Arrange
         payload = self.get_schema_for_endpoint(
-            scan_file_path=scan_directory / "circle.x3p",
             schema=schema,  # type: ignore
             mark_type=mark_type,
             mark_parameters=mark_parameters,  # type: ignore
