5c0687049af8deaff52f1f2d695e4c7dcab395a3
Sharlon N. Regales <sharlonregales@gmail.com>
2026-01-29 10:33:37 +0100

    Add log to clip factors

--- packages/scratch-core/src/renders/computations.py
+++ packages/scratch-core/src/renders/computations.py
@@ -1,4 +1,5 @@
 from container_models.base import Point
+from loguru import logger
 
 
 def clip_factors(factors: Point[float], preserve_aspect_ratio: bool) -> Point[float]:
@@ -6,5 +7,7 @@ def clip_factors(factors: Point[float], preserve_aspect_ratio: bool) -> Point[fl
     if preserve_aspect_ratio:
         max_factor = max(factors.x, factors.y)
         factors = Point(max_factor, max_factor)
-
+        logger.debug(
+            f"Preserving aspact ratio in clip factors, max point: {max_factor}"
+        )
     return Point(max(factors.x, 1.0), max(factors.y, 1.0))
