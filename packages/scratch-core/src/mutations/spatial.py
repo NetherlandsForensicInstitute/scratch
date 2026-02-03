fix wrong order of x,y

packages/scratch-core/src/mutations/spatial.py
@@ -48,7 +48,7 @@ class CropToMask(ImageMutation):
 
         :returns: True if the crop is empty, False otherwise
         """
-        return np.any(self.mask)
+        return bool(np.all(self.mask))
 
     def apply_on_image(self, scan_image: ScanImage) -> ScanImage:
         """

packages/scratch-core/tests/mutations/test_crop_image.py
@@ -57,10 +57,14 @@ class TestCropImage:
 
     def test_image_and_crop_not_equal_in_size(self, scan_image: ScanImage):
         # Arrange
-        width, height = scan_image.data.shape
         offset_size = 1
         cropping_mutator = CropToMask(
-            mask=(np.zeros((width - offset_size, height + offset_size), dtype=np.bool))
+            mask=(
+                np.zeros(
+                    (scan_image.width - offset_size, scan_image.height + offset_size),
+                    dtype=np.bool,
+                )
+            )
         )
         expected_error_message = f"image shape: {scan_image.data.shape} and crop shape: {cropping_mutator.mask.shape} are not equal"
         # Act/ Assert