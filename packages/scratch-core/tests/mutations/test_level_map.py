64c9c1f4211679faa81a2a543918cc5580d06492
Raytesnel <raymondhouwing@gmail.com>
2026-02-04 13:10:46 +0100

    add parametrize to LevelMap tests

--- packages/scratch-core/tests/mutations/test_level_map.py
+++ packages/scratch-core/tests/mutations/test_level_map.py
@@ -6,7 +6,8 @@ from container_models.scan_image import ScanImage
 from conversion.leveling.data_types import SurfaceTerms
 
 
-class TestLevelMap:
+@pytest.mark.integration
+class TestLevelMapIntegration:
     RESOURCES_DIR = (
         Path(__file__).parent.parent / "conversion" / "leveling" / "resources"
     )
@@ -17,33 +18,30 @@ class TestLevelMap:
         center_y = (scan_image.height - 1) * scan_image.scale_y * 0.5
         return center_y, center_x
 
-    @pytest.mark.integration
-    def test_map_level_sphere(self, scan_image_with_nans: ScanImage):
+    @pytest.mark.parametrize(
+        "terms, verified_file_name",
+        [
+            [SurfaceTerms.PLANE, "baseline_level_map_plane.npy"],
+            [SurfaceTerms.SPHERE, "baseline_level_map_sphere.npy"],
+        ],
+    )
+    def test_map_level(
+        self,
+        scan_image_with_nans: ScanImage,
+        verified_file_name: str,
+        terms: SurfaceTerms,
+    ):
         # Arrange
-        verified = np.load(self.RESOURCES_DIR / "baseline_level_map_sphere.npy")
+        verified = np.load(self.RESOURCES_DIR / verified_file_name)
         ycor, xcor = self.compute_image_center(scan_image=scan_image_with_nans)
         Level_map_mutator = LevelMap(
-            x_reference_point=xcor, y_reference_point=ycor, terms=SurfaceTerms.SPHERE
+            x_reference_point=xcor, y_reference_point=ycor, terms=terms
         )
         # Act
         result = Level_map_mutator(scan_image_with_nans).unwrap()
         # Assert
         assert np.allclose(result.data, verified, equal_nan=True)
 
-    @pytest.mark.integration
-    def test_map_level_plane(self, scan_image_with_nans: ScanImage):
-        # Arrange
-        verified = np.load(self.RESOURCES_DIR / "baseline_level_map_plane.npy")
-        ycor, xcor = self.compute_image_center(scan_image=scan_image_with_nans)
-        Level_map_mutator = LevelMap(
-            x_reference_point=xcor, y_reference_point=ycor, terms=SurfaceTerms.PLANE
-        )
-        # Act
-        result = Level_map_mutator(scan_image_with_nans).unwrap()
-        # Assert
-        assert np.allclose(result.data, verified, equal_nan=True)
-
-    @pytest.mark.integration
     def test_map_level_none(self, scan_image_with_nans: ScanImage):
         # Arrange
         ycor, xcor = self.compute_image_center(scan_image=scan_image_with_nans)
@@ -53,7 +51,6 @@ class TestLevelMap:
         result = Level_map_mutator(scan_image_with_nans).unwrap()
         assert np.allclose(result.data, scan_image_with_nans.data, equal_nan=True)
 
-    @pytest.mark.integration
     def test_map_level_offset(self, scan_image_with_nans: ScanImage):
         # Arrange
         ycor, xcor = self.compute_image_center(scan_image=scan_image_with_nans)
@@ -78,8 +75,6 @@ class TestLevelMap:
             [SurfaceTerms.SPHERE, (10.5, -5.2)],
             [SurfaceTerms.OFFSET, (10.5, -5.2)],
             [SurfaceTerms.DEFOCUS, (1234.567, 1234.567)],
-            [SurfaceTerms.PLANE, (1234.567, 1234.567)],
-            [SurfaceTerms.SPHERE, (1234.567, 1234.567)],
             [SurfaceTerms.ASTIG_45, (1234.567, 1234.567)],
         ],
     )
@@ -94,7 +89,8 @@ class TestLevelMap:
         Level_map_ref = LevelMap(
             x_reference_point=ref_point[0], y_reference_point=ref_point[1], terms=terms
         )
-
+        # Act
         result_centered = Level_map_mutator(scan_image_with_nans).unwrap()
         result_ref = Level_map_ref(scan_image_with_nans).unwrap()
+        # Assert
         assert np.allclose(result_centered.data, result_ref.data, equal_nan=True)
