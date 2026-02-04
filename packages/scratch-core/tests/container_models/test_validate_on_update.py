216fdb090b51642129c9f4cbd3a75da413a4d658
cfs-data <145435153+cfs-data@users.noreply.github.com>
2026-02-04 17:14:34 +0100

    fixes

--- packages/scratch-core/tests/container_models/test_validate_on_update.py
+++ packages/scratch-core/tests/container_models/test_validate_on_update.py
@@ -2,6 +2,7 @@ import pytest
 from pydantic import ValidationError
 from container_models.base import ConfigBaseModel
 from container_models.scan_image import ScanImage
+import numpy as np
 
 
 class TestModel(ConfigBaseModel):
@@ -24,14 +25,28 @@ def test_model_copy_raises_on_incorrect_fields(test_model: TestModel):
         test_model.model_copy(update={"float_field": "invalid_string"})
 
 
+def test_scan_image_model_copy_converts_updated_fields(
+    scan_image_with_nans: ScanImage,
+):
+    new_data = [[1, 2], [3, 4]]
+    updated = scan_image_with_nans.model_copy(update={"data": new_data})
+    assert np.array_equal(updated.data, new_data)
+
+
 def test_scan_image_model_copy_validates_updated_fields(
     scan_image_with_nans: ScanImage,
 ):
-    with pytest.raises(ValidationError):
+    with pytest.raises(
+        ValidationError,
+        match="Array shape mismatch, expected 2 dimension\\(s\\), but got 1",
+    ):
         scan_image_with_nans.model_copy(
             update={"data": scan_image_with_nans.data.flatten()}
         )
-    with pytest.raises(TypeError):
-        scan_image_with_nans.model_copy(update={"data": [[1, 2], [3, 4]]})
-    with pytest.raises(TypeError):
+    with pytest.raises(
+        ValidationError,
+        match="Array shape mismatch, expected 2 dimension\\(s\\), but got 0",
+    ):
         scan_image_with_nans.model_copy(update={"data": "1"})
+    with pytest.raises(ValidationError, match="Input should be an instance of ndarray"):
+        scan_image_with_nans.model_copy(update={"data": {0: 1}})
