8c105f3e168e59743ef421108f0acc3dc6ca46e9
Raytesnel <raymondhouwing@gmail.om>
2026-02-10 10:06:14 +0100

    reformat the init

--- packages/scratch-core/src/mutations/__init__.py
+++ packages/scratch-core/src/mutations/__init__.py
@@ -9,7 +9,7 @@ be applied to a `ScanImage`. Mutations are designed to be composable and
 can be chained together using a pipeline (e.g. `returns.pipeline.pipe`).
 """
 
-from .filter import LevelMap, Mask , GausianRegressionFilter
+from .filter import LevelMap, Mask, GausianRegressionFilter
 from .spatial import CropToMask, Resample
 
 
