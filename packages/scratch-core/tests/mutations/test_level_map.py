68361499cbef1245a231e2c50c46af3b04367c8a
cfs-data <145435153+cfs-data@users.noreply.github.com>
2026-02-16 16:36:14 +0100

    remove ref tests

--- packages/scratch-core/tests/mutations/test_level_map.py
+++ packages/scratch-core/tests/mutations/test_level_map.py
@@ -60,27 +60,3 @@ class TestLevelMapIntegration:
             scan_image_with_nans.data,
             equal_nan=True,
         )
-
-    @pytest.mark.parametrize(
-        "terms, ref_point",
-        [
-            [SurfaceTerms.NONE, (10.5, -5.2)],
-            [SurfaceTerms.PLANE, (10.5, -5.2)],
-            [SurfaceTerms.SPHERE, (10.5, -5.2)],
-            [SurfaceTerms.OFFSET, (10.5, -5.2)],
-            [SurfaceTerms.DEFOCUS, (1234.567, 1234.567)],
-            [SurfaceTerms.ASTIG_45, (1234.567, 1234.567)],
-        ],
-    )
-    def test_map_level_reference_point_has_no_effect(
-        self, scan_image_with_nans: ScanImage, terms: SurfaceTerms, ref_point
-    ):
-        # Arrange
-        y_center, x_center = self.compute_image_center(scan_image=scan_image_with_nans)
-        level_map_mutator = LevelMap(terms=terms)
-        Level_map_ref = LevelMap(terms=terms)
-        # Act
-        result_centered = level_map_mutator(scan_image_with_nans).unwrap()
-        result_ref = Level_map_ref(scan_image_with_nans).unwrap()
-        # Assert
-        assert np.allclose(result_centered.data, result_ref.data, equal_nan=True)
