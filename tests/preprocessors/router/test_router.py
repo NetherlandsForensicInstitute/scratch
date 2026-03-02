5823b9b1e3a56a6fe3f4914c1b449d78b706d8d3
Laurens Weijs <laurens.weijs@rijksoverheid.nl>
2026-03-02 12:57:41 +0100

    merge conflicts in test

--- tests/preprocessors/router/test_router.py
+++ tests/preprocessors/router/test_router.py
@@ -264,8 +264,6 @@ def test_edit_scan_returns_422_on_mask_shape_mismatch(
         scan_file=scan_directory / "circle.x3p",
         cutoff_length=250,
         mask_parameters=MaskParameters(shape=wrong_shape),
-        step_size_x=1,
-        step_size_y=1,
     )
 
     with monkeypatch.context() as mp:
