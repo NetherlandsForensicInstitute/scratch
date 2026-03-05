52129269c26c79f1580c08b5d1c812e1455621ce
Simone Ariens <s.ariens@nfi.nl>
2026-03-05 11:12:35 +0100

    fix profile import and httperror

--- src/processors/controller.py
+++ src/processors/controller.py
@@ -20,7 +20,9 @@ def compare_striation_marks(
         profile_compared=profile_comp,
     )
     if not mark_correlations:
-        raise HTTPException(HTTPStatus.INTERNAL_SERVER_ERROR, "No correlations were found.")
+        raise HTTPException(
+            HTTPStatus.UNPROCESSABLE_ENTITY, "profiles could not be aligned: insufficient overlap between marks"
+        )
     logger.debug("correlations are calculated")
     return mark_correlations
 
