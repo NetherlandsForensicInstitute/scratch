a10f9693d8488b598b2b1e57ddf2049cbe07d477
Raytesnel <raymondhouwing@gmail.com>
2026-03-06 15:35:42 +0100

    update api with surface options (plane/sphere) (#164)

---
+++ src/preprocessors/constants.py
@@ -0,0 +1,24 @@
+from enum import StrEnum, auto
+
+from conversion.leveling import SurfaceTerms
+
+
+class SurfaceOptions(StrEnum):
+    """
+    Surface fitting options exposed by the API.
+
+    These options represent predefined surface leveling presets
+    that map directly to internal :class:`SurfaceTerms` flag combinations.
+    """
+
+    PLANE = auto()
+    SPHERE = auto()
+    NONE = auto()
+
+    def to_surface_terms(self) -> SurfaceTerms:
+        """
+        Convert the API surface option to the corresponding internal`SurfaceTerms` flag combination.
+
+        :return: The corresponding internal surface term flags.
+        """
+        return SurfaceTerms[self.name]
