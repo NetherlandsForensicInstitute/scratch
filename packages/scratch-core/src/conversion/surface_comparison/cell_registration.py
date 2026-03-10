adfef45ba5ae0320fd2ee8acc193650001b42300
cfs-data <145435153+cfs-data@users.noreply.github.com>
2026-03-10 13:31:29 +0100

    first commit

--- packages/scratch-core/src/conversion/surface_comparison/cell_registration.py
+++ packages/scratch-core/src/conversion/surface_comparison/cell_registration.py
@@ -1,7 +1,8 @@
 from collections.abc import Iterable
 
+from container_models.base import FloatArray2D
 from container_models.scan_image import ScanImage
-from conversion.surface_comparison.grid import GridCell
+from conversion.surface_comparison.grid import GridCell, extract_patch, GridSearchParams
 from conversion.surface_comparison.models import ComparisonParams, Cell, CellMetaData
 import numpy as np
 
@@ -16,10 +17,70 @@ def _rotate_scan_image(scan_image: ScanImage, angle: float) -> ScanImage:
     )
 
 
+def _extract_patch(
+    scan_image: ScanImage,
+    coordinates: tuple[int, int],  # Top-left coordinates
+    size: tuple[int, int],  # Usually a square
+    fill_value: float = np.nan,
+) -> FloatArray2D:
+    # Compute global coordinates (of the patch in the image)
+    x1, y1 = coordinates
+    width, height = size
+    x2, y2 = x1 + width, y1 + height
+    x1, x2 = max(0, x1), min(scan_image.width, x2)
+    y1, y2 = max(0, y1), min(scan_image.height, y2)
+
+    # Extract (possibly rectangular-shaped) patch
+    patch = scan_image.data[y1:y2, x1:x2].copy()  # TODO: is copy() needed here?
+
+    # Compute local coordinates (of the patch in the cell)
+    local_x, local_y = 0, 0  # TODO: Implement this computation
+
+    # Generate (square-shaped) padded output
+    output = np.empty(shape=size, dtype=np.float64)
+    output.fill(fill_value)
+    output[local_y : local_y + patch.shape[0], local_x : local_x + patch.shape[1]] = (
+        patch
+    )
+
+    return output
+
+
+def _find_best_translation(
+    scan_image: ScanImage,
+    cell: GridCell,
+    min_fill_fraction: float,
+    nan_fill_value: float,
+) -> tuple[float, int, int]:
+    # TODO: Implement correct implementation
+    if np.isnan(cell.cell_data).any():
+        raise ValueError("Cell data cannot contain NaNs.")
+
+    max_score = float("-inf")
+    best_x, best_y = 0, 0
+    for y in range(scan_image.height - cell.height // 2):
+        for x in range(scan_image.width - cell.width // 2):
+            patch = extract_patch(
+                scan_image=scan_image, coordinates=(x, y), size=cell.size
+            )
+            fill_fraction = 1 - np.isnan(patch).sum() / patch.size
+            if fill_fraction < min_fill_fraction:
+                # If not enough data, go to next point
+                continue
+            patch = patch.copy()
+            # Replace NaNs with fill value so that we can compute a score
+            patch[np.isnan(patch)] = nan_fill_value
+            score = np.corrcoef(cell.cell_data, patch)
+            if score > max_score:
+                best_x, best_y = x, y
+                max_score = score
+    return max_score, best_x, best_y
+
+
 def _pixels_to_meters(
     coordinates: tuple[int, int], pixel_size: tuple[float, float]
 ) -> tuple[float, float]:
-    # TODO: Remove this function if possible?
+    # TODO: remove this function if possible?
     return coordinates[0] * pixel_size[0], coordinates[1] * pixel_size[1]
 
 
@@ -27,10 +88,42 @@ def coarse_registration(
     grid_cells: Iterable[GridCell],
     comparison_image: ScanImage,
     params: ComparisonParams,
-    fill_value_reference: float,  # Fill value for NaNs in the grid cell data
+    fill_value_reference: float,
 ) -> list[Cell]:
-    # TODO: Implement this
+    # TODO: Write docstring
+    angle = params.search_angle_min
+    fill_value_comparison = float(np.nanmean(comparison_image))
     pixel_size = comparison_image.scale_x, comparison_image.scale_y
+
+    while angle < params.search_angle_max:
+        # Rotate the comparison image by `-angle` degrees.
+        # This is equivalent to rotating the reference patch by `angle` degrees.
+        rotated = _rotate_scan_image(scan_image=comparison_image, angle=-angle)
+
+        for grid_cell in grid_cells:
+            # TODO: Rewrite and optimize this part
+            # Deep copy cell and fill NaN values with numerical value
+            # We cannot use `grid_cell` directly yet for this since we neet its fill fraction later
+            cell_copy = GridCell(
+                center=grid_cell.center,
+                size=grid_cell.size,
+                cell_data=grid_cell.cell_data.copy(),
+                grid_search_params=GridSearchParams(),
+            )
+            cell_copy.fill_nans(fill_value=fill_value_reference)
+
+            score, x, y = _find_best_translation(
+                scan_image=rotated,
+                cell=cell_copy,
+                min_fill_fraction=params.minimum_fill_fraction,  # TODO: use different fill fraction for comparison
+                nan_fill_value=fill_value_comparison,
+            )
+            if score > grid_cell.grid_search_params.score:
+                # Update search parameters
+                grid_cell.grid_search_params.update(score=score, angle=angle, x=x, y=y)
+
+        angle += params.search_angle_step
+
     output = []
     for grid_cell in grid_cells:
         cell = Cell(
