5abe4836f7d290077866f82f29fbe20ea4c32014
cfs-data <145435153+cfs-data@users.noreply.github.com>
2026-03-12 19:23:15 +0100

    added todo

--- packages/scratch-core/src/conversion/surface_comparison/cell_registration/utils.py
+++ packages/scratch-core/src/conversion/surface_comparison/cell_registration/utils.py
@@ -14,7 +14,8 @@ def convert_grid_cell_to_cell(grid_cell: GridCell, pixel_size: float) -> Cell:
             values=grid_cell.center, pixel_size=pixel_size
         ),
         cell_data=grid_cell.cell_data,
-        # TODO: add cell size in meters here
+        # TODO: Do we actually need cell data here?
+        # TODO: Add the cell size in meters as well
         fill_fraction_reference=grid_cell.fill_fraction,
         best_score=grid_cell.grid_search_params.score,
         angle_deg=grid_cell.grid_search_params.angle,
@@ -25,10 +26,10 @@ def convert_grid_cell_to_cell(grid_cell: GridCell, pixel_size: float) -> Cell:
             ),
             pixel_size=pixel_size,
         ),
-        is_congruent=False,  # TODO: We shouldn't set this here
+        is_congruent=False,  # TODO: We shouldn't set this here?
         meta_data=CellMetaData(
             is_outlier=False, residual_angle_deg=0.0, position_error=(0, 0)
-        ),  # TODO: We shouldn't set this here
+        ),  # TODO: We shouldn't set this here?
     )
     return cell
 
