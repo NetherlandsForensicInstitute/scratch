134abf57f530fed606999bcb66f165ae9039d786
cfs-data <145435153+cfs-data@users.noreply.github.com>
2026-07-29 10:33:19 +0200

    Remove unused constants

--- packages/scratch-core/tests/conversion/leveling/constants.py
+++ packages/scratch-core/tests/conversion/leveling/constants.py
@@ -1,11 +1,4 @@
 from pathlib import Path
 
-from conversion.data_formats import SurfaceTerms
-
-SINGLE_TERMS = list(SurfaceTerms)
-COMBINED_TERMS = [SurfaceTerms.PLANE, SurfaceTerms.SPHERE]
-SINGLE_AND_COMBINED_TERMS = SINGLE_TERMS + COMBINED_TERMS
-
-
 TEST_ROOT = Path(__file__).parent
 RESOURCES_DIR = TEST_ROOT / "resources"
