15db0f2b4f02b34322167be91b075c6e4a8bad7c
cfs-data <145435153+cfs-data@users.noreply.github.com>
2026-08-04 12:53:59 +0200

    ruff quality check

--- packages/scratch-core/tests/conversion/surface_comparison/cell_registration/conftest.py
+++ packages/scratch-core/tests/conversion/surface_comparison/cell_registration/conftest.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pytest
 from _pytest.fixtures import SubRequest
 
@@ -42,7 +41,6 @@ def identical_match_inputs(
         CELL_TOP_LEFT[1] : CELL_TOP_LEFT[1] + CELL_SIZE,
         CELL_TOP_LEFT[0] : CELL_TOP_LEFT[0] + CELL_SIZE,
     ]
-    nan_fill_value = float(np.nanmean(scan_image.data))  # TODO: Do we want this value?
     grid_cell = make_grid_cell(data=cell_data, top_left=CELL_TOP_LEFT)
     params = identity_params(cell_size_px=CELL_SIZE, pixel_size=PIXEL_SIZE)
     return [grid_cell], scan_image, params
